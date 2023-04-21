76aa6f6aa22626e13212a063bd2c35eb138ec382
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-04-21 12:02:40 -0300

    Craido o crud de Enderecos

--- main/api/rotas_urls.py
+++ main/api/rotas_urls.py
@@ -1,7 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from .db import Enderecos
 
 bp_urls = Blueprint('bp_urls', __name__)
 
-@bp_urls.route('/urls')
-def api_index():
-    return 'Estou em Rotas de Urls !!'
+@bp_urls.route('/urls', methods=['GET'])
+def getUrls():
+    return jsonify(Enderecos)
+
+@bp_urls.route('/urls/<int:id>', methods=['GET'])
+def getEnderecoById(id):
+    for endereco in Enderecos:
+        if endereco.get('id') == id:
+            return jsonify(endereco)
+        
+@bp_urls.route('/urls', methods=['POST'])
+def criarEndereco():
+    post = request.get_json()
+
+    new_endereco = {
+        "id": len(Enderecos) + 1,
+        "nome_do_sistema": post.get('nome_do_sistema'),
+        "endereco": post.get('endereco'),
+        "ping": post.get('ping')
+    }
+
+    Enderecos.append(new_endereco)
+
+    return jsonify(new_endereco)
+
+@bp_urls.route('/urls/<int:id>', methods=['PUT'])
+def editarEndereco(id):
+    post = request.get_json()
+
+    att_endereco = {
+        "id": id,
+        "nome_do_sistema": post.get('nome_do_sistema'),
+        "endereco": post.get('endereco'),
+        "ping": post.get('ping')
+    }
+
+    for index, endereco in enumerate(Enderecos):
+        if endereco.get('id') == id:
+            Enderecos[index] = att_endereco
+            return jsonify(Enderecos[index])
+
+@bp_urls.route('/urls/<int:id>', methods=['DELETE'])
+def deletarEndereco(id):
+    for endereco in Enderecos:
+        if endereco.get('id') == id:
+            Enderecos.remove(endereco)
+            return jsonify(endereco)
+
