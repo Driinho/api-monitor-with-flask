8908570c62872904575ef708d4ee499ffc992dcf
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-04-24 18:47:56 -0300

    Alterado o crud de endereços para utilizar o banco de dados

--- src/api/rotas_urls.py
+++ src/api/rotas_urls.py
@@ -1,10 +1,11 @@
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restx import Resource
 from .db import Enderecos
 
 from src.models.Enderecos import endereco
 from src.models.Enderecos import endereco_post
 from src.server.instance import server
+from src.models.models import db, Endereco
 
 app, api = server.app, server.api
 
@@ -13,7 +14,8 @@ class RotasEnderecos(Resource):
     # método GET para listar todos os endereços
     @api.marshal_list_with(endereco)
     def get(self):
-        return Enderecos, 200
+        todos_enderecos = Endereco.query.all()
+        return todos_enderecos, 200
     
     # método POST para criar um novo endereço
     @api.expect(endereco_post, validate=True)
@@ -21,14 +23,14 @@ class RotasEnderecos(Resource):
     def post(self):
         post = request.get_json()
 
-        new_endereco = {
-            "id": len(Enderecos) + 1,
-            "nome_do_sistema": post.get('nome_do_sistema'),
-            "endereco": post.get('endereco'),
-            "ping": post.get('ping')
-        }
+        new_endereco = Endereco(
+            nome_do_sistema=post.get('nome_do_sistema'), 
+            endereco=post.get('endereco'), 
+            ping=post.get('ping')
+        )
 
-        Enderecos.append(new_endereco)
+        db.session.add(new_endereco)
+        db.session.commit()
 
         return new_endereco, 201
 
@@ -36,11 +38,14 @@ class RotasEnderecos(Resource):
     @api.route('/api/enderecos/<int:id>')
     class endereco_by_id(Resource):
         # método GET para listar um endereço específico
-        @api.marshal_list_with(endereco)    
+        @api.marshal_with(endereco)
         def get(self, id):
-            for endereco in Enderecos:
-                if endereco.get('id') == id:
-                    return endereco
+            endereco_por_id = Endereco.query.filter_by(id=id).first()
+
+            if endereco_por_id is None:
+                return endereco_por_id, 404
+            else:
+                return endereco_por_id, 200
                 
         # método PUT para editar um endereço específico
         @api.expect(endereco_post, validate=True)
@@ -54,18 +59,21 @@ class RotasEnderecos(Resource):
                 "endereco": post.get('endereco'),
                 "ping": post.get('ping')
             }
+            
+            if db.session.query(Endereco).filter_by(id=id).update(att_endereco) == 0:
+                db.session.commit()
+                return att_endereco, 404
 
-            for index, endereco in enumerate(Enderecos):
-                if endereco.get('id') == id:
-                    Enderecos[index] = att_endereco
-                    return Enderecos[index]
-        
+            db.session.commit()
+            return att_endereco, 200
+
         # método DELETE para deletar um endereço específico
         @api.marshal_list_with(endereco)
         def delete(self, id):
-            for endereco in Enderecos:
-                if endereco.get('id') == id:
-                    endereco_aux = endereco
-                    Enderecos.remove(endereco)
-                    return endereco_aux
+            del_endereco = Endereco.query.filter_by(id=id).first()
+
+            db.session.query(Endereco).filter_by(id=id).update({"status": False})
+            db.session.commit()
+
+            return del_endereco, 204
 
--- src/models/models.py
+++ src/models/models.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Endereco(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome_do_sistema = db.Column(db.String(80), nullable=False)
     endereco = db.Column(db.String(120), nullable=False)
     ping = db.Column(db.Boolean, nullable=False)
+    status = db.Column(db.String(120), default=True)
 
 class Incidente(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     endereco = db.Column(db.String(120), nullable=False)
     data_hora_queda = db.Column(db.DateTime, nullable=False)
     data_hora_retorno = db.Column(db.DateTime, nullable=False)
